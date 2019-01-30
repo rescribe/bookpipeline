3e31e5f58b89c2f71c8a306950173bab221dc565
Nick White <git@njw.name>
2019-01-30 21:22:32 +0000

    Set window size automatically based on resolution

--- binarize/main.go
+++ binarize/main.go
@@ -14,21 +14,22 @@ import (
 	"os"
 )
 
+// TODO: do more testing to see how good this assumption is
+func autowsize(bounds image.Rectangle) int {
+	return bounds.Dx() / 60
+}
+
 func main() {
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage: binarize [-w num] [-k num] inimg outimg\n")
 		flag.PrintDefaults()
 	}
-	wsize := flag.Int("w", 31, "Window size for sauvola algorithm (needs to be odd)")
-        ksize := flag.Float64("k", 0.5, "K for sauvola algorithm")
+	wsize := flag.Int("w", 0, "Window size for sauvola algorithm. Set automatically based on resolution if not set.")
+	ksize := flag.Float64("k", 0.5, "K for sauvola algorithm. This controls the overall threshold level. Set it lower for very light text (try 0.1 or 0.2).")
 	flag.Parse()
 	if flag.NArg() < 2 {
 		flag.Usage()
 		os.Exit(1)
-	}
-
-	if *wsize % 2 == 0 {
-		*wsize++
 	}
 
 	f, err := os.Open(flag.Arg(0))
@@ -42,18 +43,28 @@ func main() {
 	}
 	b := img.Bounds()
 	gray := image.NewGray(image.Rect(0, 0, b.Dx(), b.Dy()))
-        draw.Draw(gray, b, img, b.Min, draw.Src)
+	draw.Draw(gray, b, img, b.Min, draw.Src)
 
-	// TODO: estimate an appropriate window size based on resolution
+	if *wsize == 0 {
+		*wsize = autowsize(b)
+		log.Printf("Set window size to %d\n", *wsize)
+	}
+
+	if *wsize % 2 == 0 {
+		*wsize++
+	}
+
+	// TODO: come up with a way to set a good ksize automatically
+
 	thresh := IntegralSauvola(gray, *ksize, *wsize)
 
 	f, err = os.Create(flag.Arg(1))
-        if err != nil {
+	if err != nil {
 		log.Fatalf("Could not create file %s: %v\n", flag.Arg(1), err)
-        }
+	}
 	defer f.Close()
 	err = png.Encode(f, thresh)
-        if err != nil {
+	if err != nil {
 		log.Fatalf("Could not encode image: %v\n", err)
-        }
+	}
 }
