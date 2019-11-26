798455070f30a63641a089b3f619a2fe8101475f
Nick White <git@njw.name>
2019-11-26 16:42:46 +0000

    Ensure error in file walking is correctly returned

--- cmd/pdfbook/main.go
+++ cmd/pdfbook/main.go
@@ -82,6 +82,9 @@ func addBest(dir string, pdf Pdfer, colour, smaller bool) error {
 // the pdf for each one.
 func walker(pdf Pdfer, colour, smaller bool) filepath.WalkFunc {
 	return func(fpath string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
 		if info.IsDir() {
 			return nil
 		}
