22d2c1ad5a832c388e40b662f74934c33e440fa1
Nick White <git@njw.name>
2020-05-04 17:37:44 +0100

    Update spot image id

--- cloudsettings.go
+++ cloudsettings.go
@@ -19,7 +19,7 @@ package bookpipeline
 // TODO: create profile and security group with mkpipeline
 const (
 	spotProfile = "arn:aws:iam::557852942063:instance-profile/pipeliner"
-	spotImage   = "ami-0bc6ef6900f6da5d3"
+	spotImage   = "ami-05aec2524d0793ce2"
 	spotType    = "m5.large"
 	spotSg      = "sg-0be8a3ab89e7136b9"
 )
