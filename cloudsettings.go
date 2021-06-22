99175ac316657e3339c6adf66637a96c78a34603
Nick White <git@njw.name>
2021-06-22 14:20:27 +0100

    cloud: update spot image to latest version that wont attempt to build rescribe tool

--- cloudsettings.go
+++ cloudsettings.go
@@ -19,7 +19,7 @@ package bookpipeline
 // TODO: create profile and security group with mkpipeline
 const (
 	spotProfile = "arn:aws:iam::557852942063:instance-profile/pipeliner"
-	spotImage   = "ami-06ccaf3c8dc2ee243"
+	spotImage   = "ami-0381d644372cace2a"
 	spotType    = "m5.large"
 	spotSg      = "sg-0be8a3ab89e7136b9"
 )
