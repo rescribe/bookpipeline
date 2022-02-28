65be1a688b9213e9e020ef00fe5726f18404a99b
Nick White <git@njw.name>
2022-02-28 17:05:28 +0000

    rescribe: Add embed_darwin.go to include getbook into OSX builds

---
+++ cmd/rescribe/embed_darwin.go
@@ -0,0 +1,10 @@
+// Copyright 2021 Nick White.
+// Use of this source code is governed by the GPLv3
+// license that can be found in the LICENSE file.
+
+package main
+
+import _ "embed"
+
+//go:embed getgbook-darwin-cac42fb.zip
+var gbookzip []byte
